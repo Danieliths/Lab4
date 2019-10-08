60fafc9d9ec5afaf3e15c5f73b1a9bb2f9e5e9b7
Daniel <daniel.andersson@iths.se>
2019-10-08 12:16:47 +0200

    Broke out map creating to a new class

---
+++ source/repos/Lab4/Lab4/MapCreator.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+
+namespace Lab4
+{
+    class MapCreator
+    {
+        public MapCreator() { }        
+        public void CreateMapAndObjectsFromString(string symbols, int collum, GameManager gameManager)
+        {
+            char[] charArray = new char[symbols.Length];
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                using (StringReader sr = new StringReader(symbols))
+                {
+                    sr.Read(charArray, 0, symbols.Length);
+                    switch (charArray[i])
+                    {
+                        case '#':
+                            gameManager.Map[i, collum] = new Wall(new Point(i, collum));
+                            break;
+                        case ' ':
+                            gameManager.Map[i, collum] = new Tile(new Point(i, collum));
+                            break;
+                        case 'R':
+                            gameManager.Map[i, collum] = new Wall(new Point(i, collum));
+                            gameManager.Map[i, collum].Revealed = true;
+                            break;
+                        case 'A':
+                            gameManager.Map[i, collum] = new Door(new Point(i, collum), Color.Red);
+                            break;
+                        case 'B':
+                            gameManager.Map[i, collum] = new Door(new Point(i, collum), Color.Blue);
+                            break;
+                        case 'C':
+                            gameManager.Map[i, collum] = new Door(new Point(i, collum), Color.Yellow);
+                            break;
+                        default:
+                            break;
+                    }
+                    switch (charArray[i])
+                    {
+                        case '@':
+                            gameManager.Map[i, collum] = new Tile(new Point(i, collum));
+                            gameManager.Player.Location = new Point(i, collum);
+                            gameManager.GameObject.Add(gameManager.Player);
+                            break;
+                        case 'a':
+                            gameManager.Map[i, collum] = new Tile(new Point(i, collum));
+                            gameManager.GameObject.Add(new Key(new Point(i, collum), Color.Red));
+                            break;
+                        case 'b':
+                            gameManager.Map[i, collum] = new Tile(new Point(i, collum));
+                            gameManager.GameObject.Add(new Key(new Point(i, collum), Color.Blue));
+                            break;
+                        case 'c':
+                            gameManager.Map[i, collum] = new Tile(new Point(i, collum));
+                            gameManager.GameObject.Add(new Key(new Point(i, collum), Color.Yellow));
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }           
+        } 
+        public void CrateMap(GameManager gameManager)
+        {
+            CreateMapAndObjectsFromString("RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR", 0, gameManager);
+            CreateMapAndObjectsFromString("RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR", 1, gameManager);
+            CreateMapAndObjectsFromString("RR                    ###                       ###                                               RR", 2, gameManager);
+            CreateMapAndObjectsFromString("RR                    ###                       ###                                               RR", 3, gameManager);
+            CreateMapAndObjectsFromString("RR            a       ###        b               C                                                RR", 4, gameManager);
+            CreateMapAndObjectsFromString("RR                    ###                       ###                                               RR", 5, gameManager);
+            CreateMapAndObjectsFromString("RR                    ###                       ##################################################RR", 6, gameManager);
+            CreateMapAndObjectsFromString("RR                    ###                       ##################################################RR", 7, gameManager);
+            CreateMapAndObjectsFromString("RR                    ###                       ##################################################RR", 8, gameManager);
+            CreateMapAndObjectsFromString("RR       @            ###                       ###                                               RR", 9, gameManager);
+            CreateMapAndObjectsFromString("RR                    ###                        B                                                RR", 10, gameManager);
+            CreateMapAndObjectsFromString("RR                    ###                       ###                             c                 RR", 11, gameManager);
+            CreateMapAndObjectsFromString("RR                     A                        ###                                               RR", 12, gameManager);
+            CreateMapAndObjectsFromString("RR                    ###                       ###                                               RR", 13, gameManager);
+            CreateMapAndObjectsFromString("RR                    ###                       ###                                               RR", 14, gameManager);
+            CreateMapAndObjectsFromString("RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR", 15, gameManager);
+            CreateMapAndObjectsFromString("RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR", 16, gameManager);           
+        }        
+    }
+}
--- source/repos/Lab4/Lab4/Program.cs
+++ source/repos/Lab4/Lab4/Program.cs
@@ -14,87 +14,16 @@ namespace Lab4
             RenderMap renderMap = new RenderMap();
             Input input = new Input();
             Movement movement = new Movement();
-            player.Inventory = new List<GameObject>();            
-            void CreateMapAndObjectsFromString(string symbols, int collum)
-            {
-                char[] charArray = new char[symbols.Length];
-                for (int row = 0; row < symbols.Length; row++)
-                {
-                    using (StringReader sr = new StringReader(symbols))
-                    {
-                        sr.Read(charArray, 0, symbols.Length);
-                        switch (charArray[row])
-                        {
-                            case '#':
-                                map[row, collum] = new Wall(new Point(row, collum));
-                                break;
-                            case ' ':
-                                map[row, collum] = new Tile(new Point(row, collum));
-                                break;
-                            case 'R':
-                                map[row, collum] = new Wall(new Point(row, collum));
-                                map[row, collum].Revealed = true;
-                                break;
-                            case 'A':
-                                map[row, collum] = new Door(new Point(row, collum), Color.Red);
-                                break;
-                            case 'B':
-                                map[row, collum] = new Door(new Point(row, collum), Color.Blue);
-                                break;
-                            case 'C':
-                                map[row, collum] = new Door(new Point(row, collum), Color.Yellow);
-                                break;
-                            default:
-                                break;
-                        }
-                        switch (charArray[row])
-                        {
-                            case '@':
-                                map[row, collum] = new Tile(new Point(row, collum));
-                                player.Location = new Point(row, collum);
-                                gameObjekt.Add(player);
-                                break;
-                            case 'a':
-                                map[row, collum] = new Tile(new Point(row, collum));
-                                gameObjekt.Add(new Key(new Point(row, collum), Color.Red));
-                                break;
-                            case 'b':
-                                map[row, collum] = new Tile(new Point(row, collum));
-                                gameObjekt.Add(new Key(new Point(row, collum), Color.Blue));
-                                break;
-                            case 'c':
-                                map[row, collum] = new Tile(new Point(row, collum));
-                                gameObjekt.Add(new Key(new Point(row, collum), Color.Yellow));
-                                break;
-                            default:
-                                break;
-                        }                       
-                    }
-                }
-            } // Bryt ut from main så att den inte är nästlad i main
-            CreateMapAndObjectsFromString("RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR", 0);
-            CreateMapAndObjectsFromString("RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR", 1);
-            CreateMapAndObjectsFromString("RR                    ###                       ###                                               RR", 2);
-            CreateMapAndObjectsFromString("RR                    ###                       ###                                               RR", 3);
-            CreateMapAndObjectsFromString("RR            a       ###        b               C                                                RR", 4);
-            CreateMapAndObjectsFromString("RR                    ###                       ###                                               RR", 5);
-            CreateMapAndObjectsFromString("RR                    ###                       ##################################################RR", 6);
-            CreateMapAndObjectsFromString("RR                    ###                       ##################################################RR", 7);
-            CreateMapAndObjectsFromString("RR                    ###                       ##################################################RR", 8);
-            CreateMapAndObjectsFromString("RR       @            ###                       ###                                               RR", 9);
-            CreateMapAndObjectsFromString("RR                    ###                        B                                                RR", 10);
-            CreateMapAndObjectsFromString("RR                    ###                       ###                             c                 RR", 11);
-            CreateMapAndObjectsFromString("RR                     A                        ###                                               RR", 12);
-            CreateMapAndObjectsFromString("RR                    ###                       ###                                               RR", 13);
-            CreateMapAndObjectsFromString("RR                    ###                       ###                                               RR", 14);
-            CreateMapAndObjectsFromString("RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR", 15);
-            CreateMapAndObjectsFromString("RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR", 16);
+            MapCreator mapCreator = new MapCreator();
             GameManager gameManager = new GameManager();
+            player.Inventory = new List<GameObject>();                        
             gameManager.GameObject = gameObjekt;
             gameManager.Player = player;
             gameManager.Map = map;            
+            mapCreator.CrateMap(gameManager);
             renderMap.UpdateAllPoints(gameManager);
             renderMap.PrintAroundPlayer(gameManager);
+
 
             while (true)
             {
