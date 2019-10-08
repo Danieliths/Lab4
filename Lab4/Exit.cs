f3125a7ebbc14239a6833ba89f853c8a1e81dfb3
Daniel <daniel.andersson@iths.se>
2019-10-08 22:27:12 +0200

    added exit class and new color

---
+++ Lab4/Exit.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab4
+{
+    class Exit : Construkt
+    {
+        public Exit(Point location)
+        {
+            Symbol = 'E';
+            Revealed = false;
+            CrossAble = true;
+            Location = location;
+            ConstruktColor = Color.Green;
+        }        
+    }
+}
--- Lab4/GameManager.cs
+++ Lab4/GameManager.cs
@@ -4,7 +4,7 @@ using System.IO;
 namespace Lab4
 {
     enum Direction { Upp, Down, Right, Left, None }
-    enum Color { Red, Blue, Gray, Yellow }  
+    enum Color { Red, Blue, Gray, Yellow, Green }  
     class GameManager
     {        
         public Player Player { get; set; }
